b20ba2966cebf85dcadd1040f969413cd78f41f7
c7x43t <c7x43t@gmail.com>
2019-09-29 13:58:16 +0200

    update

--- angular-cooking-app/RecipeApp/backend/app.js
+++ angular-cooking-app/RecipeApp/backend/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-
+const fs = require("fs");
 const Post = require("./models/post");
 const config = JSON.parse(fs.readFileSync('./config.json'));
 const app = express();
