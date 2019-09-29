2a4e2ae41a1b7caf5bb6a019c6f12896a42eeaed
Thoedel <onyxcodehub@protonmail.com>
2019-09-29 16:22:25 +0200

    update

--- angular-cooking-app/RecipeApp/src/app/app.module.ts
+++ angular-cooking-app/RecipeApp/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
+import { MatSelectModule } from "@angular/material";
 import {
   MatInputModule,
   MatCardModule,
