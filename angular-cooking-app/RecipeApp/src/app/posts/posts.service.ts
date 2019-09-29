744666bc60deb9b173b78d717714cfaa6cb2ffcf
c7x43t <c7x43t@gmail.com>
2019-09-29 14:12:38 +0200

    update

--- angular-cooking-app/RecipeApp/src/app/posts/posts.service.ts
+++ angular-cooking-app/RecipeApp/src/app/posts/posts.service.ts
@@ -39,7 +39,7 @@ export class PostsService {
   addPost(title: string, content: string) {
     const post: Post = { id: null, title: title, content: content };
     this.http
-      .post<{ message: string, postId: string }>("http://localhost:80/api/posts", post)
+      .post<{ message: string, postId: string }>("http://157.230.100.170:80/api/posts", post)
       .subscribe(responseData => {
         const id = responseData.postId;
         post.id = id;
@@ -49,7 +49,7 @@ export class PostsService {
   }
 
   deletePost(postId: string) {
-    this.http.delete("http://localhost:80/api/posts/" + postId)
+    this.http.delete("http://157.230.100.170:80/api/posts/" + postId)
       .subscribe(() => {
         const updatedPosts = this.posts.filter(post => post.id !== postId);
         this.posts = updatedPosts;
